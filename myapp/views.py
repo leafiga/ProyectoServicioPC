b26d0123bd622d9f3ad85a1eb762ff4a187a40e9
Figueroa Leandro <figueroavictor333@gmail.com>
2026-06-16 21:27:13 -0300

    Agrega requirements

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context = {"mensaje":"Ofrecemos servicios de reparación de computadoras, mantenimiento y soporte técnico."}
+    return render(request,"myapp/index.html",context)
+
+
+
+
