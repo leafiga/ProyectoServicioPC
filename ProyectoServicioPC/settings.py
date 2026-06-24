a8fd2fec6224767b236aed056e8a1047458e4182
Figueroa Leandro <figueroavictor333@gmail.com>
2026-06-23 22:04:43 -0300

    Continuando con django; templates

--- ProyectoServicioPC/settings.py
+++ ProyectoServicioPC/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ProyectoServicioPC.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR /"templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
