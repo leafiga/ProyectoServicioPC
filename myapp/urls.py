3b1a7b27886f6b6edc47e75bb7838ea452a8b68d
Figueroa Leandro <figueroavictor333@gmail.com>
2026-06-24 22:34:50 -0300

    agregando vistas y urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+app_name = "myapp"
+
 urlpatterns = [
     path('', views.index, name='index'),
+    path('clientes/', views.clientes, name = 'clientes')
+   
+
+   
+    
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
+from .models import Cliente, Reparacion, Equipo, Tecnico
 
 
 # Create your views here.
 def index(request):
     context = {"mensaje":"Ofrecemos servicios de reparación de computadoras, mantenimiento y soporte técnico."}
     return render(request,"myapp/index.html",context)
+
+def clientes(request):
+    clientes = clientes.objects.all()
+    return render (request, "myapp/clientes.hmtl", {"clientes": clientes})
+
+def equipos(request):
+    equipos = equipos.objects.all()
+    return render (request, "myapp/equipos.hmtl", {"equipos": equipos})
 
 
 
